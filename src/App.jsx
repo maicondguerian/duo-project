904c6c03f84c8bbd591f6aeb32d2062f5e6ef167
maicondguerian <maicondguerian@gmail.com>
2023-03-01 19:32:11 -0300

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import RightSide from './Components/rightSide/rightSide'
 import LeftSide from './Components/leftSide/leftSide'
 import { HiArrowRight } from "react-icons/hi2";
+import NextPage from './Components/nextPage/NextPage';
 function App({Icon}) {
   return (
     <main className="App">
       <RightSide Icon={HiArrowRight}/>
       <LeftSide />
+      {/* <NextPage /> */}
     </main>
   )
 }
---
+++ src/Components/nextPage/NextPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../rightSide/righrSide.module.css'
+function NextPage() {
+  return (
+    <main className={styles.cardWrapper}>
+        <section>
+          
+        </section>
+    </main>
+  )
+}
+
+export default NextPage;
