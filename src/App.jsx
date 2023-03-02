0456f8b670a78a3f2e3effe74c3d2a8b4d2a9d5f
maicondguerian <maicondguerian@gmail.com>
2023-03-02 15:34:31 -0300

    state

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import './App.css'
 import RightSide from './Components/rightSide/rightSide'
 import LeftSide from './Components/leftSide/leftSide'
 import { HiArrowRight } from "react-icons/hi2";
-import NextPage from './Components/nextPage/NextPage';
-function App({Icon}) {
+function App() {
   return (
     <main className="App">
       <LeftSide />
-      <RightSide Icon={HiArrowRight}/>
-      {/* <NextPage /> */}
+      <RightSide Icon={HiArrowRight} />
       <div className='footer'>
       <code>
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
--- src/Components/rightSide/rightSide.jsx
+++ src/Components/rightSide/rightSide.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import styles from "./righrSide.module.css";
 function RightSide({ Icon }) {
 
@@ -6,9 +6,22 @@ function RightSide({ Icon }) {
   const iconReaction = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="#F55" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.25" d="M10.833 8.333V2.5l-6.666 9.167h5V17.5l6.666-9.167h-5Z" /></svg>
   const iconVerbal = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="#00BB8F" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.25" d="M7.5 10h5M10 18.333A8.333 8.333 0 1 0 1.667 10c0 1.518.406 2.942 1.115 4.167l-.699 3.75 3.75-.699A8.295 8.295 0 0 0 10 18.333Z" /></svg>
   const iconVisual = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke="#1125D6" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.25" d="M10 11.667a1.667 1.667 0 1 0 0-3.334 1.667 1.667 0 0 0 0 3.334Z" /><path stroke="#1125D6" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.25" d="M17.5 10c-1.574 2.492-4.402 5-7.5 5s-5.926-2.508-7.5-5C4.416 7.632 6.66 5 10 5s5.584 2.632 7.5 5Z" /></svg>
+
+  const [showAfterClass, setShowAfterClass] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowAfterClass(true);
+    }, 2005);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
     <>
-      <section className={styles.cardWrapper}>
+      <section className={`${styles.cardWrapper} ${showAfterClass ? styles.cardWrapperAfter : ''}`}>
         <div>
           <h2> Sumary</h2>
         </div>
